a4cda5dec8bea04e4f93be19efdcaea2b3e87631
rabuu <rasbuur@gmail.com>
2021-03-23 20:49:17 +0100

    Add lock logo

--- config.def.h
+++ config.def.h
@@ -20,21 +20,46 @@ static const int timetocancel = 5;
 static const int monitortime = 10;
 
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
-static const int logosize = 50;
-static const int logow = 12;	/* grid width and height for right center alignment*/
-static const int logoh = 6;
+static const int logosize = 40;
 
-static XRectangle rectangles[9] = {
-	/* x	y	w	h */
-	{ 0,	3,	1,	3 },
-	{ 1,	3,	2,	1 },
-	{ 0,	5,	8,	1 },
-	{ 3,	0,	1,	5 },
-	{ 5,	3,	1,	2 },
-	{ 7,	3,	1,	2 },
-	{ 8,	3,	4,	1 },
-	{ 9,	4,	1,	2 },
-	{ 11,	4,	1,	2 },
+/* grid width and height for right center alignment*/
+/* static const int logow = 12; // dwm */
+/* static const int logoh = 6; // dwm */
+
+static const int logow = 11; // lock
+static const int logoh = 13; // lock
+
+static XRectangle rectangles[12] = {
+	/* x, y, 	w, h */
+
+	// dwm
+	/* { 0,	3,	1,	3 }, */
+	/* { 1,	3,	2,	1 }, */
+	/* { 0,	5,	8,	1 }, */
+	/* { 3,	0,	1,	5 }, */
+	/* { 5,	3,	1,	2 }, */
+	/* { 7,	3,	1,	2 }, */
+	/* { 8,	3,	4,	1 }, */
+	/* { 9,	4,	1,	2 }, */
+	/* { 11,	4,	1,	2 }, */
+
+
+	// lock
+	{ 4, 0, 	3, 2 },
+	{ 3, 1, 	5, 1 },
+	{ 2, 2, 	2, 3 },
+	{ 7, 2, 	2, 3 },
+
+	{ 1, 5, 	9, 1 },
+	{ 1, 12, 	9, 1 },
+
+	{ 0, 6, 	4, 6 },
+	{ 7, 6, 	4, 6 },
+
+	{ 4, 6, 	3, 1 },
+	{ 4, 10, 	3, 2 },
+	{ 4, 9, 	1, 1 },
+	{ 6, 9, 	1, 1 },
 
 };
 
